c0ff1083c42c232e74826f287a3abae377a86634
Merijn68 <merijn.vanmiltenburg@hotmail.com>
2021-12-30 12:13:51 +0100

    Data Preperation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,27 +1,27 @@
-from datetime import date
 import pandas as pd
-from pandas.core.tools.datetimes import DatetimeScalarOrArrayConvertible
 import numpy as np
+#from pandas.core.tools.datetimes import Datetime
+
 import sys
 from pathlib import Path
 from typing import Tuple, Union
-
 from loguru import logger
-from sklearn.model_selection import train_test_split
+
 
 
 sys.path.insert(0, "..")
 
-country_dict = {'DE': 'Germany','FR': 'France','ES': 'Spain','IT': 'Italy','NL': 'Netherlands'}
+
 
 
 def read_csv(
     path: Path = Path("../data/raw/bonds.csv"),
+    thousands:str = ','    
 ) -> pd.DataFrame:
     logger.info(f"Loading data from {path}")
     df = pd.DataFrame()
     try:
-        df = pd.read_csv(path, thousands=',' )
+        df = pd.read_csv(path, thousands=thousands )
 
     except Exception as error:      
         logger.error(f"Error loading data: {error}")
@@ -34,65 +34,62 @@ def get_bond_data(
     path: Path = Path("../data/raw/bonds.csv"),
 ) -> pd.DataFrame:
     logger.info('Load bond data')
-    '''        
-        Inlezen bond definities, imputeren lege waarden en omzetten naar categorische of float variabelen
-
-        Berekenen:
-            CFI Code splitsen per letter
-            Oorsponkelijke looptijd berekenen (maturity date - issue date)
-    '''
-    # loading bond data    
+    ''' Read raw bond data from CSV, drop columns and format data '''    
+    
+    # loading bond data        
     df = read_csv(path)
-
-    # Subset data on country name
-    df['country_name'] = df['country_name'].str.strip()
-    df = df[df['country_name'].isin(country_dict.values())]
-
-
-    # Lets select only EUR bonds - we don't want to complicate things with FX volatility
-    df['ccy'] = df['ccy'].astype('string')
     
-    if ccy:
-        df = df[df['ccy'] == ccy]
+    # Drop unneeded columns
+    df = df.drop(['bondname','bond_ext_name', 'group_name','fix_float','cparty_type','CO2_factor'], axis = 1)
 
-    df.drop(['bondname','bond_ext_name', 'group_name','fix_float','cparty_type','CO2_factor'], axis = 1, inplace = True)
-    
-    df['issue_dt']= pd.to_datetime(df['issue_dt'])
-    df['first_coupon_date'] = pd.to_datetime(df['first_coupon_date'])
-    df['mature_dt']= pd.to_datetime(df['mature_dt'])
-    
+    # Correct columns types
+    for column in ['cfi_code','isin','ccy','issuer_name','coupon_frq','country_name','issue_rating']:
+        df[column] = df[column].astype('string')
+
+    for column in ['issue_dt','first_coupon_date','mature_dt' ]:
+        df[column] = pd.to_datetime(df[column])
+       
     # Remove trailing spaces
     df['isin'] = df['isin'].str.strip()
-    
-
-    df = df.set_index('isin')
+    df['country_name'] = df['country_name'].str.strip()    
     
     # Total Issue amount '-' should be converted to 0
     df = df.rename(columns={' tot_issue ': 'tot_issue'})  
     df.loc[df['tot_issue'] == '-', 'tot_issue'] = '0'  
     df['tot_issue']= df['tot_issue'].str.replace(',','').str.replace('-','0').astype('float')    
+    
+    return df
 
-    df['issuer_name'] = df['issuer_name'].astype('category')
-    df['coupon_frq'] = df['coupon_frq'].astype('category')
 
-    df['country_name'] = df['country_name'].astype('category')
+# Subset data on country name and currency
+def subset_bonds(
+        df: pd.DataFrame,
+        country: dict,
+        ccy: str = 'EUR'         
+) -> pd.DataFrame:
+    
+    df = df[df['country_name'].isin(country_dict.values())]
+    # Lets select only EUR bonds - we don't want to complicate things with FX volatility
+           
+    if ccy:
+        df = df[df['ccy'] == ccy]
 
+    return df
+
+
+def impute_bonds(
+     df: pd.DataFrame,
+) -> pd.DataFrame:
+
+    logger.info('Impute bond data')
     # FillNa Issue Rating missing for US and Wallonie    
     selection = ( df['issuer_name'] == 'UNITED STATES TREASURY' )
     df.loc[selection,'issue_rating'] = df.loc[selection,'issue_rating'].fillna('AAA')
     selection = ( df['issuer_name'] == 'WALLONIE' )
     df.loc[selection,'issue_rating'] = df.loc[selection,'issue_rating'].fillna('AA-')
-    df['issue_rating'] = df['issue_rating'].astype('category')  
-    
+
     # CFI Codes Imputed with Dept Instrument XXXXX (Unknown)
     df['cfi_code'] = df['cfi_code'].fillna('DXXXXX')
-
-    df['cfi_code1'] = df['cfi_code'].str[:1].astype('category')
-    df['cfi_code2'] = df['cfi_code'].str[1:2].astype('category')
-    df['cfi_code3'] = df['cfi_code'].str[2:3].astype('category')
-    df['cfi_code4'] = df['cfi_code'].str[3:4].astype('category')
-    df['cfi_code5'] = df['cfi_code'].str[4:5].astype('category')
-    df['cfi_code6'] = df['cfi_code'].str[5:6].astype('category')        
 
     # Date 1899-12-30 means no first coupon date was known...
     df.loc[df['first_coupon_date'] == '30-12-1899', 'first_coupon_date'] = None    
@@ -103,10 +100,17 @@ def get_bond_data(
     df['first_coupon_date'] = df['first_coupon_date'].fillna(df['issue_dt'])
     df['bond_duration'] = df['mature_dt'] - df['first_coupon_date']    
 
-    # Store processed data
-    df.to_csv('../data/processed/bonds.csv')
-
     return df
+    
+def save_bonds(
+    df: pd.DataFrame,    
+) -> pd.DataFrame:
+    # Store processed data
+    logger.info('Save preprocessed bond data')
+    try:
+        df.to_csv('../data/processed/bonds.csv')
+    except Exception as error:      
+        logger.error(f"Error saving data: {error}")
 
 # For sampling business Days
 #    from pandas.tseries.offsets import BDay
@@ -125,49 +129,86 @@ def get_price(
     logger.info('Load bond price data')
     
     df = read_csv(path)
-    df['reference_identifier'] = df['reference_identifier'].astype('string')
+
+    # Correct columns types
+    for column in ['reference_identifier','ccy']:
+        df[column] = df[column].astype('string')
+    for column in ['rate_dt' ]:
+        df[column] = pd.to_datetime(df[column])
 
     # Filter on referenceId
-    if ids:
-        df = df[df['reference_identifier'].isin(ids)]
+    #if ids:
+    #    df = df[df['reference_identifier'].isin(ids)]
 
-    df['ccy'] = df['ccy'].astype('string')
-    df['rate_dt'] =  pd.to_datetime(df['rate_dt'])
+    # Bid en Offer zijn overbodig want gelijk in bron systeem.
+    # Deze voegen we samen
     df['mid'] = (df['bid'] + df['offer'])  / 2
     df = df.drop(['bid','offer'], axis = 1)    
-
-    # 99.999 is a 'default price'. Lets drop these.    
-    df = df[df['mid'] != 99.999]    
     
     return df
+
+def impute_price(
+     df: pd.DataFrame,
+) -> pd.DataFrame:
+
+    logger.info('Impute bond price')
+    # 99.999 is een 'default'. Deze verwijderen we
+    df = df[df['mid'] != 99.999]    
+    return df
+
 
 def get_yield_curves(
     path: Path = Path("../data/raw/yield.csv"),
 ) -> pd.DataFrame:
-    logger.info('Load goverment yield data')
+    logger.info('Load goverment yield curve data')
+
+    country_dict = {'DE': 'Germany','FR': 'France','ES': 'Spain','IT': 'Italy','NL': 'Netherlands'}
+
     df = read_csv(path)
-    df['name']= df['name'].astype('string')
-    df['ccy'] = df['ccy'].astype('string')
-    df['rate_dt'] =  pd.to_datetime(df['rate_dt'])
-    df['timeband'] = df['timeband'].astype('string')
-    df['actual_dt'] =  pd.to_datetime(df['actual_dt'])
-    df['int_basis'] = df['int_basis'].astype('string')
+
+    # Voeg een country_name kolom toe
+    for country in country_dict:
+        df.loc[df['name'].str.contains(country), 'country'] = country_dict[country]
+
+    # hernoem name naar ratename (voor de duidelijkheid)
     df = df.rename(columns={'name': 'ratename'})
 
+    for column in ['ratename','ccy','timeband','int_basis','country']:
+        df[column] = df[column].astype('string')
+    for column in ['rate_dt','actual_dt' ]:
+        df[column] = pd.to_datetime(df[column])
+    
+    return df    
+
+def impute_yield_curves(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+
+    logger.info('Impute yield curve')
+    
     # Drop MM curves only Bond Based curves are actually correct
     df = df[df['ratename'].str.contains('BB')]    
     
+    return df
 
-    # Create a country_name column to match with bond data    
-    for country in country_dict:
-        df.loc[df['ratename'].str.contains(country), 'country'] = country_dict[country]
-    df['country']= df['country'].astype('string')
+def save_yield_curves(
+    df: pd.DataFrame,    
+):
+    logger.info('Save preprocessed yield curve data')
+    try:
+        df.to_csv('../data/processed/yield.csv')
+    except Exception as error:      
+        logger.error(f"Error saving data: {error}")
 
-    return df    
+    
 
 
-
-
+def make_data(
+):
+    ''' Generate all data preprocessing '''
+    df_bonds = get_bond_data()
+    df_bonds = impute_bonds(df_bonds)
+    save_bonds(df_bonds)
 
 
 
