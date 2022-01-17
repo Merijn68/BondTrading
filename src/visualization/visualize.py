4f71d5219737db0468d3dc80b62aac942864c571
Merijn68 <merijn.vanmiltenburg@hotmail.com>
2022-01-17 11:56:56 +0100

    visualizatie

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import Boolean
 import  seaborn as sns
 from    matplotlib import pyplot as plt
 import  pandas as pd
@@ -8,15 +9,38 @@ from typing import List, Tuple, Dict, Optional
 
 def countplot(
     data: pd.DataFrame, 
-    x: str,
-    hue: str = ''    
+    x: List[str],
+    subplots: Boolean = True, 
+    ncols: int = 2,    
+    maxitems = 10, 
+    other_category = True,
+    title: str = '',  
 ):
 
-    if hue:
-        ax = sns.countplot(data = data, y = x, hue = hue, order = data[x].value_counts().index)
+    if subplots:
+        number_of_items = len(x)
+        nrows = math.ceil(number_of_items / ncols)
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
+
+        
+        if title:
+            fig.suptitle(title)
+
+        for item, ax in zip (x, axes.flatten()):
+
+            ax.title.set_text(item)
+            s = data[item]             
+            if maxitems:                   
+                d = s.value_counts()[:maxitems]
+                sns.barplot(x=d.values, y=d.index, ax = ax, orient = 'h')                    
+            else:                
+                d = s.value_counts()               
+                sns.barplot(x=d.values, y=d.index, ax = ax, orient = 'h')
+                            
+            ax.set(xlabel="", ylabel = "")
     else:
-        ax = sns.countplot(data = data, y = x, order = data[x].value_counts().index)
-    ax.set(xlabel="", ylabel = "")
+        ax = sns.countplot(data = data, y = x[0], order = data[x[0]].value_counts().index)
+        ax.set(xlabel="", ylabel = "")
 
 def grouped_lineplot(
     data: pd.DataFrame,    
@@ -72,4 +96,45 @@ def lplot(
  
         sns.lineplot(data = line, palette = [palletes[x]])
         x = x + 1
-        
+
+def lineplot(
+    data: pd.DataFrame,    
+    x: str,
+    y: str,    
+    hue: str = '',
+    figsize: Tuple[int, int] = (15, 8),    
+) -> plt.Axes:  
+
+    plt.figure(figsize=figsize)    
+    ax = sns.lineplot(data = data, x = x, y = y, hue =  hue)
+
+    return ax
+
+
+def scatterplot(
+    data: pd.DataFrame,    
+    x: str,
+    y: str,    
+    hue: str = '',
+    label: str = '',
+    figsize: Tuple[int, int] = (15, 8),
+) -> plt.Axes:  
+    
+    plt.figure(figsize=figsize)    
+    ax = sns.scatterplot(data = data, x = x, y = y, hue = hue, label = label)
+
+    return ax
+
+def boxplot(
+    data: pd.DataFrame,    
+    x: str,
+    y: str,            
+    order: List[str] = [],
+    figsize: Tuple[int, int] = (15, 8),
+) -> plt.Axes:  
+    
+    plt.figure(figsize=figsize)        
+    ax = sns.boxplot(data = data, x = x, y = y, order = order)    
+    
+
+    return ax
