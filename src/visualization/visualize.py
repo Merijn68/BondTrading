9d23989aca397e1f56e6d6b028a3a81b5e372d95
Merijn68 <merijn.vanmiltenburg@hotmail.com>
2022-01-22 15:01:51 +0100

    visualizatie distributieplot

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -43,32 +43,32 @@ def countplot(
         ax = sns.countplot(data = data, y = x[0], order = data[x[0]].value_counts().index)
         ax.set(xlabel="", ylabel = "")
 
-def grouped_lineplot(
-    data: pd.DataFrame,    
-    ncols: int,
-    x: str,    
-    y: str,
-    hue: str,
-    group: str, 
-    title: str = '',  
-    figsize: Tuple[int, int] = (8, 6),  
-) -> plt.Figure:
+# def grouped_lineplot(
+#     data: pd.DataFrame,    
+#     ncols: int,
+#     x: str,    
+#     y: str,
+#     hue: str,
+#     group: str, 
+#     title: str = '',  
+#     figsize: Tuple[int, int] = (8, 6),  
+# ) -> plt.Figure:
 
-    grouplist = data[group].unique()
-    nrows = math.ceil(len(grouplist) / ncols)
-    print (ncols, nrows)
+#     grouplist = data[group].unique()
+#     nrows = math.ceil(len(grouplist) / ncols)
+#     print (ncols, nrows)
 
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols,sharey=True, sharex=True, figsize=figsize)
+#     fig, axes = plt.subplots(nrows=nrows, ncols=ncols,sharey=True, sharex=True, figsize=figsize)
     
-    if title:
-        fig.suptitle(title)
+#     if title:
+#         fig.suptitle(title)
     
-    for item, ax, in zip(grouplist, axes.flatten()):                          
-            ax.title.set_text(item)
-            selection = data[group] == item
-            sns.lineplot(ax=ax, data=data[selection], x=x, y=y, hue = hue, legend = None)
+#     for item, ax, in zip(grouplist, axes.flatten()):                          
+#             ax.title.set_text(item)
+#             selection = data[group] == item
+#             sns.lineplot(ax=ax, data=data[selection], x=x, y=y, hue = hue, legend = None)
             
-    return fig
+#     return fig
 
 
 def boxplot(
@@ -86,17 +86,17 @@ def boxplot(
     
     # plt.xticks(rotation=90)
 
-def lplot(
-    data: List[pd.DataFrame],        
-    figsize: Tuple[int, int] = (8, 6),
-) -> None:    
-    palletes = ['green', 'blue', 'red']
-    plt.figure(figsize=figsize)    
-    x = 0
-    for line in data:           
+# def lplot(
+#     data: List[pd.DataFrame],        
+#     figsize: Tuple[int, int] = (8, 6),
+# ) -> None:    
+#     palletes = ['green', 'blue', 'red']
+#     plt.figure(figsize=figsize)    
+#     x = 0
+#     for line in data:           
  
-        sns.lineplot(data = line, palette = [palletes[x]])
-        x = x + 1
+#         sns.lineplot(data = line, palette = [palletes[x]])
+#         x = x + 1
 
 def lineplot(
     data: pd.DataFrame,    
@@ -137,6 +137,21 @@ def boxplot(
     plt.figure(figsize=figsize)        
     ax = sns.boxplot(data = data, x = x, y = y, order = order)    
     
+    return ax
+
+def distribution(
+    data: pd.DataFrame,
+    x: str,    
+    xlabel: str = 'Prijs',
+    ylabel: str = 'Aantal',
+    figsize: Tuple[int, int] = (15, 8),
+) -> plt.Axes:  
+
+    plt.figure(figsize=figsize) 
+    ax = sns.histplot(data, x = x)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
     return ax
 
 def plot_example(
